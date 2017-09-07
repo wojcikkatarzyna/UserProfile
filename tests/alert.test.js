23f47e984f59bb614df2fbc2a76bcad5bd106d27
Katarzyna Wojcik <wojcik.katarzyna.danuta@gmail.com>
2017-09-07 08:10:34 +0200

    remove incorrect test

--- tests/alert.test.js
+++ tests/alert.test.js
@@ -2,7 +2,6 @@ import { shallow, mount, render } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Alert from '../js/Components/Alert.jsx';
-import UserInfo from '../js/Components/UserInfo.jsx';
 
 describe('Alert', () => {
     it('renders without crashing', () => {
@@ -10,9 +9,3 @@ describe('Alert', () => {
         ReactDOM.render(<Alert/>, div);
     });
 });
-
-const alert = shallow(<Alert />);
-const Info = shallow(<UserInfo />);
-expect(Info.state().displayAlert).toBe(false);
-alert.find('button').simulate('click');
-expect(Info.state().displayAlert).toBe(true);
